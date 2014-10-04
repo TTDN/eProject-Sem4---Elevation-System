99388007877883853e3d20093cb1395ba7e9d051
TTDN <triminh.it@gmail.com>
2014-10-04 16:17:30 +0700

    Update lại 2 file DAO.
    Update lại 2 file DAO của tui làm hôm qua.

--- Elevation_System/src/dao/ContactUsDAO.java
+++ Elevation_System/src/dao/ContactUsDAO.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import dto.ContactUsDTO;
 
@@ -33,9 +35,38 @@ public class ContactUsDAO {
 		return false;
 	}
 
-	public ContactUsDTO FindbyName(String name) {
+	public List<ContactUsDTO> FindAll(){
+		
+		ArrayList<ContactUsDTO> listcontactus = new ArrayList<ContactUsDTO>();
+		
+		try{
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			Connection conn = (Connection) DriverManager
+					.getConnection("jdbc:sqlserver://localhost:1433;databaseName=Elevation System;ader=sa;password=1234567;");
+			PreparedStatement ps = conn
+					.prepareStatement("SELECT * FROM ContactUs");
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				
+				ContactUsDTO cu = new ContactUsDTO();
+				cu.setID_ContactUs(rs.getInt("ID_ContactUs"));
+				cu.setName(rs.getString("Name"));
+				cu.setTelephone(rs.getString("Telephone"));
+				cu.setCompanyName(rs.getString("CompanyName"));
+				cu.setContents(rs.getString("Contents"));
+				listcontactus.add(cu);
+			}
+		}catch(SQLException | ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		
+		return listcontactus;
+	}
+	
+	public List<ContactUsDTO> FindByName(String name) {
 
-		ContactUsDTO cu = new ContactUsDTO();
+		ArrayList<ContactUsDTO> listcontactus = new ArrayList<ContactUsDTO>();
 
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -46,21 +77,25 @@ public class ContactUsDAO {
 			ps.setString(1, name);
 			ResultSet rs = ps.executeQuery();
 			if (rs.next()) {
+				
+				ContactUsDTO cu = new ContactUsDTO();
 				cu.setID_ContactUs(rs.getInt("ID_ContactUs"));
 				cu.setName(rs.getString("Name"));
 				cu.setTelephone(rs.getString("Telephone"));
 				cu.setCompanyName(rs.getString("CompanyName"));
 				cu.setContents(rs.getString("Contents"));
+				
+				listcontactus.add(cu);
 			}
 		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		return cu;
+		return listcontactus;
 	}
 
-	public ContactUsDTO FindbyCompanyName(String companyName) {
+	public List<ContactUsDTO> FindByCompanyName(String companyName) {
 
-		ContactUsDTO cu = new ContactUsDTO();
+		ArrayList<ContactUsDTO> listcontactus = new ArrayList<ContactUsDTO>();
 
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -71,15 +106,19 @@ public class ContactUsDAO {
 			ps.setString(1, companyName);
 			ResultSet rs = ps.executeQuery();
 			if (rs.next()) {
+				
+				ContactUsDTO cu = new ContactUsDTO();
 				cu.setID_ContactUs(rs.getInt("ID_ContactUs"));
 				cu.setName(rs.getString("Name"));
 				cu.setTelephone(rs.getString("Telephone"));
 				cu.setCompanyName(rs.getString("CompanyName"));
 				cu.setContents(rs.getString("Contents"));
+				
+				listcontactus.add(cu);
 			}
 		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		return cu;
+		return listcontactus;
 	}
 }
--- Elevation_System/src/dao/ProductDAO.java
+++ Elevation_System/src/dao/ProductDAO.java
@@ -7,38 +7,44 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ProductDAO{
-	
-	public boolean InsertProduct(ProductDTO p){
-		
-		try{
+public class ProductDAO {
+
+	public boolean InsertProduct(ProductDTO p) {
+
+		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			Connection conn = (Connection) DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=Elevation System;ader=sa;password=1234567;");
-			PreparedStatement ps = conn.prepareStatement("INSERT INTO ProductDAO (ID_Type,Name,Images,Description,Status,Contents) VALUES(?,?,?,?,?,?)");
+			Connection conn = (Connection) DriverManager
+					.getConnection("jdbc:sqlserver://localhost:1433;databaseName=Elevation System;ader=sa;password=1234567;");
+			PreparedStatement ps = conn
+					.prepareStatement("INSERT INTO ProductDAO (ID_Type,Name,Images,Description,Status,Contents) VALUES(?,?,?,?,?,?)");
 			ps.setInt(1, p.getID_Type());
 			ps.setString(2, p.getName());
 			ps.setString(3, p.getImages());
 			ps.setString(4, p.getDescription());
 			ps.setBoolean(5, p.isStatus());
 			ps.setString(6, p.getContents());
-		
+
 			int kq = ps.executeUpdate();
 			if (kq == 1) {
 				return true;
 			}
-		}catch(SQLException | ClassNotFoundException e){
+		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 		return false;
 	}
-	
-	public boolean UpdateProduct(ProductDTO p){
-		
-		try{
+
+	public boolean UpdateProduct(ProductDTO p) {
+
+		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			Connection conn = (Connection) DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=Elevation System;ader=sa;password=1234567;");
-			PreparedStatement ps = conn.prepareStatement("UPDATE ProductDAO SET ID_Type=?,Name=?,Images=?,Description=?,Status=?,Contents=? WHERE ID_Product=?");
+			Connection conn = (Connection) DriverManager
+					.getConnection("jdbc:sqlserver://localhost:1433;databaseName=Elevation System;ader=sa;password=1234567;");
+			PreparedStatement ps = conn
+					.prepareStatement("UPDATE ProductDAO SET ID_Type=?,Name=?,Images=?,Description=?,Status=?,Contents=? WHERE ID_Product=?");
 			ps.setInt(1, p.getID_Type());
 			ps.setString(2, p.getName());
 			ps.setString(3, p.getImages());
@@ -49,32 +55,69 @@ public class ProductDAO{
 			if (kq == 1) {
 				return true;
 			}
-		}catch (SQLException | ClassNotFoundException e) {
+		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 		return false;
 	}
-	
-	public ProductDTO FindbyName(String name){
-		
-		ProductDTO p = new ProductDTO();
-		try{
+
+	public List<ProductDTO> FindAll() {
+
+		ArrayList<ProductDTO> listproduct = new ArrayList<ProductDTO>();
+
+		try {
+
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			Connection conn = (Connection) DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=Elevation System;ader=sa;password=1234567;");
-			PreparedStatement ps = conn.prepareStatement("SELECT * FROM ProductDTO WHERE Name = ?");
-			ps.setString(1, name);
+			Connection conn = (Connection) DriverManager
+					.getConnection("jdbc:sqlserver://localhost:1433;databaseName=Elevation System;ader=sa;password=1234567;");
+			PreparedStatement ps = conn
+					.prepareStatement("SELECT * FROM ProductDTO");
 			ResultSet rs = ps.executeQuery();
 			if (rs.next()) {
+
+				ProductDTO p = new ProductDTO();
 				p.setID_Type(rs.getInt("ID_Type"));
 				p.setName(rs.getString("Name"));
 				p.setImages(rs.getString("Images"));
 				p.setDescription(rs.getString("Description"));
 				p.setStatus(rs.getBoolean("Status"));
 				p.setContents(rs.getString("Contents"));
+				listproduct.add(p);
 			}
-		}catch (SQLException | ClassNotFoundException e) {
+		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		return p;
+
+		return listproduct;
+	}
+	
+	public List<ProductDTO> FindByName(String name) {
+
+		ArrayList<ProductDTO> listproduct = new ArrayList<ProductDTO>();
+
+		try {
+
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			Connection conn = (Connection) DriverManager
+					.getConnection("jdbc:sqlserver://localhost:1433;databaseName=Elevation System;ader=sa;password=1234567;");
+			PreparedStatement ps = conn
+					.prepareStatement("SELECT * FROM ProductDTO WHERE Name = ?");
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+
+				ProductDTO p = new ProductDTO();
+				p.setID_Type(rs.getInt("ID_Type"));
+				p.setName(rs.getString("Name"));
+				p.setImages(rs.getString("Images"));
+				p.setDescription(rs.getString("Description"));
+				p.setStatus(rs.getBoolean("Status"));
+				p.setContents(rs.getString("Contents"));
+				listproduct.add(p);
+			}
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		return listproduct;
 	}
 }
