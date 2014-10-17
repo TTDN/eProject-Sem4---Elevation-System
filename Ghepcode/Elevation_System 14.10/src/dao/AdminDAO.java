902845ee504784a3c97f25a9b5e40abbb024b9a6
TTDN <triminh.it@gmail.com>
2014-10-17 14:13:25 +0700

    Thêm phần admin.
    Thêm:
    +Add Admin (AdminManage)
    +Đổi profile (AdminChangeProfile)
    +Change password (AdminChangePassword)
    +Login (AdminLogin)
    +Update thêm trong AdminDAO và AdminBean.

--- Ghepcode/Elevation_System 14.10/src/dao/AdminDAO.java
+++ Ghepcode/Elevation_System 14.10/src/dao/AdminDAO.java
@@ -7,24 +7,70 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.List;
+
+import util.HttpUtil;
 import dto.AdminDTO;
 
-
 public class AdminDAO {
+	public String getSession() {
+		return String.valueOf(HttpUtil.getFromSession("username"));
+	}
+
+	public String DestroySession() {
+		HttpUtil.putToSession("username", null);
+		return "Index";
+	}
+
 	public static boolean InsertAdmin(AdminDTO ad) {
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			Connection conn = (Connection) DriverManager
 					.getConnection("jdbc:sqlserver://localhost:1433;databaseName=ElevationSystem;user=sa;password=1234567;");
+			// Check username.
 			PreparedStatement ps = conn
-					.prepareStatement("INSERT INTO Admin (UserName,PassWord,FullName,Phone,Email,Address) VALUES(?,?,?,?,?,?)");
+					.prepareStatement("SELECT * FROM Admin WHERE ID_Admin = ?");
 			ps.setString(1, ad.getUserName());
-			ps.setString(2, ad.getPassWord());
-			ps.setString(3, ad.getFullname());
-			ps.setString(4,ad.getPhone());
-			ps.setString(5,ad.getEmail());
-			ps.setString(6,ad.getAddress());
-		
+			ResultSet rs = ps.executeQuery();
+			if (rs == null && !rs.next()) {
+				HttpUtil.putToSession("AdminAddAdminNotification",
+						"Username already exists.");
+				return false;
+			} else {
+				ps = conn
+						.prepareStatement("INSERT INTO Admin (UserName,PassWord,FullName,Phone,Email,Address) VALUES(?,?,?,?,?,?)");
+				ps.setString(1, ad.getUserName());
+				ps.setString(2, ad.getPassWord());
+				ps.setString(3, ad.getFullname());
+				ps.setString(4, ad.getPhone());
+				ps.setString(5, ad.getEmail());
+				ps.setString(6, ad.getAddress());
+				int kq = ps.executeUpdate();
+				if (kq == 1) {
+					HttpUtil.putToSession("AdminAddAdminNotification", "Success!");
+					return true;
+				}
+			}
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		HttpUtil.putToSession("AdminLoginAlert", "Error?!");
+		return false;
+	}
+
+	public static boolean ChangeProfileAdmin(String fullname, String phone, String email, String address, int id) {
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			Connection conn = (Connection) DriverManager
+					.getConnection("jdbc:sqlserver://localhost:1433;databaseName=ElevationSystem;user=sa;password=1234567;");
+			PreparedStatement ps = conn
+					.prepareStatement("UPDATE Admin SET FullName=?,Phone=?,Email=?,Address=? WHERE ID_Admin=?");
+			ps.setString(1, fullname);
+			ps.setString(2, phone);
+			ps.setString(3, email);
+			ps.setString(4, address);
+			ps.setInt(5, id);
+
 			int kq = ps.executeUpdate();
 			if (kq == 1) {
 				return true;
@@ -35,21 +81,15 @@ public class AdminDAO {
 		return false;
 	}
 
-	
-	public boolean updateAdmin(AdminDTO ad) {
+	public static boolean ChangePasswordAdmin(String password, int id) {
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			Connection conn = (Connection) DriverManager
 					.getConnection("jdbc:sqlserver://localhost:1433;databaseName=ElevationSystem;user=sa;password=1234567;");
 			PreparedStatement ps = conn
-					.prepareStatement("UPDATE Admin SET UserName=?,PassWord=?,FullName=?,Phone=?,Email=?,Address=? WHERE ID_Admin=?");
-			ps.setString(1, ad.getUserName());
-			ps.setString(2, ad.getPassWord());
-			ps.setString(3, ad.getFullname());
-			ps.setString(4,ad.getPhone());
-			ps.setString(5,ad.getEmail());
-			ps.setString(6,ad.getAddress());
-			ps.setInt(7, ad.getID_Admin());
+					.prepareStatement("UPDATE Admin SET PassWord=? WHERE ID_Admin=?");
+			ps.setString(1, password);
+			ps.setInt(2, id);
 
 			int kq = ps.executeUpdate();
 			if (kq == 1) {
@@ -80,7 +120,7 @@ public class AdminDAO {
 		return false;
 	}
 
-	public ArrayList<AdminDTO> findallAdmin() {
+	public static ArrayList<AdminDTO> FindAllAdmin() {
 		ArrayList<AdminDTO> listad = new ArrayList<AdminDTO>();
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -97,7 +137,7 @@ public class AdminDAO {
 				ad.setPhone(rs.getString("Phone"));
 				ad.setEmail(rs.getString("Email"));
 				ad.setAddress(rs.getString("Address"));
-				
+
 				listad.add(ad);
 			}
 		} catch (SQLException | ClassNotFoundException e) {
@@ -124,7 +164,7 @@ public class AdminDAO {
 				ad.setPhone(rs.getString("Phone"));
 				ad.setEmail(rs.getString("Email"));
 				ad.setAddress(rs.getString("Address"));
-				
+
 			}
 		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
@@ -132,5 +172,36 @@ public class AdminDAO {
 		return ad;
 	}
 
+	public static Boolean Login(String username, String password) {
 
+		List<AdminDTO> listadmin = FindAllAdmin();
+		for (AdminDTO a : listadmin) {
+			if (a.getUserName().equals(username)
+					&& a.getPassWord().equals(password)) {
+				HttpUtil.putToSession("IDAdmin", a.getID_Admin());
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static Boolean ChangeProfile(String fullname, String phone, String email, String address, int id) {
+
+		Boolean b = ChangeProfileAdmin(fullname, phone, email, address, id);
+		if (b) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public static Boolean ChangePassword(String password, int id){
+		
+		Boolean b = ChangePasswordAdmin(password, id);
+		if (b) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
--- Ghepcode/Elevation_System 14.10/src/managerbean/AdminBean.java
+++ Ghepcode/Elevation_System 14.10/src/managerbean/AdminBean.java
@@ -1,35 +1,134 @@
 package managerbean;
-import java.sql.SQLException;
+
 import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+
+import util.HttpUtil;
 import dao.AdminDAO;
 import dto.AdminDTO;
 
-@ManagedBean(name ="registeradmin")
+@ManagedBean(name = "AdminBean")
 public class AdminBean {
 	public static final long serialVersionUID = 1L;
-	private AdminDTO ad= new AdminDTO();
+	private AdminDTO ad = new AdminDTO();
 	private List<AdminDTO> listAdmin;
+	public String PP;
+	public String NP;
+	public String RP;
+
+	public String getPP() {
+		return PP;
+	}
+
+	public void setPP(String pP) {
+		PP = pP;
+	}
+
+	public String getNP() {
+		return NP;
+	}
+
+	public void setNP(String nP) {
+		NP = nP;
+	}
+
+	public String getRP() {
+		return RP;
+	}
+
+	public void setRP(String rP) {
+		RP = rP;
+	}
+
 	public AdminDTO getAd() {
 		return ad;
 	}
+
 	public void setAd(AdminDTO ad) {
 		this.ad = ad;
 	}
+
 	public List<AdminDTO> getListAdmin() {
 		return listAdmin;
 	}
+
 	public void setListAdmin(List<AdminDTO> listAdmin) {
 		this.listAdmin = listAdmin;
 	}
+
 	public AdminBean() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public String register() throws ClassNotFoundException, SQLException {
-	AdminDAO.InsertAdmin(ad);
 
-		return "register-success?faces-redirect=true";
+	// Copy từ đây trở xuống.
+	public String getSessionAdminLoginNotification() {
+		return String
+				.valueOf(HttpUtil.getFromSession("AdminLoginNotification"));
 	}
 
+	public String getSessionID_Admin() {
+		return String.valueOf(HttpUtil.getFromSession("IDAdmin"));
+	}
+
+	public String LogOut_DestroySessionAdminUsername() {
+		HttpUtil.putToSession("AdminUsername", null);
+		return "AdminLogin";
+	}
+
+	public String Login() {
+		Boolean b = AdminDAO.Login(ad.getUserName(), ad.getPassWord());
+		if (b == true) {
+			HttpUtil.putToSession("AdminUsername", ad.getUserName());
+			return "AdminManage";
+		} else {
+			HttpUtil.putToSession("AdminLoginNotification",
+					"Username or password incorrect, please check and try again.");
+		}
+		return null;
+	}
+
+	public String AddAdmin() {
+
+		Boolean b = AdminDAO.InsertAdmin(ad);
+
+		if (b) {
+			HttpUtil.putToSession("AdminAddAdminNotification", "Success!");
+		} else {
+			HttpUtil.putToSession("AdminAddAdminNotification",
+					"Error, please check and try again.");
+		}
+		return "AdminManage";
+	}
+
+	public String ChangeProfile() {
+
+		Boolean b = AdminDAO.ChangeProfile(ad.getFullname(), ad.getPhone(),
+				ad.getEmail(), ad.getAddress(), ad.getID_Admin());
+
+		if (b) {
+			HttpUtil.putToSession("AdminChangeProfileNotification", "Success!");
+		} else {
+			HttpUtil.putToSession("AdminChangeProfileNotification",
+					"Error, please check and try again.");
+		}
+		return null;
+	}
+
+	public String ChangePassword() {
+
+		if (NP.equals(RP)) {
+			if (AdminDAO.ChangePassword(NP,
+					Integer.parseInt(getSessionID_Admin()))) {
+				// success
+			} else {
+				// fail
+			}
+		} else {
+			HttpUtil.putToSession("AdminChangePasswordNotification",
+					"Error, please check and try again.");
+		}
+		return null;
+	}
 }
